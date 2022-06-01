0e2beee186147d859f53b2f4a8b861c445c99722
Binotz <binotto.riccardo@gmail.com>
2022-06-01 15:21:05 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+//Data collection
+const userAge = parseInt(prompt("quanti anni hai?"));
+const userKilometers = parseInt(prompt("Quanti kilometri devi percorrere?"));
+
+//variable definition
+const euroPerKm = 0.21;
+const childDiscount = 0.2;
+const elderDiscount = 0.4;
+let msg = "";
+
+//price calc
+let price = userKilometers * euroPerKm;
+
+if (userAge < 18){ 
+    price -= (price * childDiscount);
+    msg = `Per la tua giovane età, pagherai solamente ${price} &euro;`;
+}
+if (userAge > 65){
+    price -= (price * elderDiscount);
+    msg = `Per gli over65, il prezzo è di solamente ${price} &euro;`;
+}
+
+//output
+document.getElementById("ticket").innerHTML = msg;
+console.log(price.toFixed(2));
