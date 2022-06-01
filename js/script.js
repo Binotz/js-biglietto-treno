d98b79454d0d1b39651f51f5b56da050561ea94b
Binotz <binotto.riccardo@gmail.com>
2022-06-01 15:48:18 +0200

    Easter egg 2.0

--- js/script.js
+++ js/script.js
@@ -6,18 +6,24 @@ const userKilometers = parseInt(prompt("Quanti kilometri devi percorrere?"));
 const euroPerKm = 0.21;
 const childDiscount = 0.2;
 const elderDiscount = 0.4;
-let msg = "Oh oh, hai rotto qualcosa... adesso il Dev si arrabbierà molto!";
+let msg = "";
 
 //price calc
 let price = userKilometers * euroPerKm;
 
-if (userAge < 18){ 
+//check if the fields were properly filled
+if(isNaN(userAge) || isNaN(userKilometers)){
+    msg = "Oh oh, hai rotto qualcosa... adesso il Dev si arrabbierà molto!";
+} 
+//user age check
+else if (userAge < 18){ 
     price -= (price * childDiscount);
     msg = `Per la tua giovane età, pagherai solamente ${price} &euro;`;
-}
-if (userAge > 65){
+} else if (userAge > 65){
     price -= (price * elderDiscount);
     msg = `Per gli over65, il prezzo è di solamente ${price} &euro;`;
+} else {
+    msg = `il prezzo è di ${price} &euro;`;
 }
 
 //output
